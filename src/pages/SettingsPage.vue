cc61fa8d335cced76e6fab73e8e269d31194a7b1
maksi <maksimbaltak@yandex.ru>
2023-03-13 15:44:07 +0200

    init project

---
+++ src/pages/SettingsPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>settings</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,29 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: '/settings'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/settings',
+    name: 'settings',
+    component: () => import('@/pages/SettingsPage.vue')
+  },
+  {
+    path: '/top',
+    name: 'top',
+    component: () => import('@/pages/TopPage.vue')
+  },
+  {
+    path: '/board',
+    name: 'board',
+    component: () => import('@/pages/GameBoardPage.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'notFound',
+    component: () => import('@/pages/NotFoundPage.vue')
   }
 ]
 
